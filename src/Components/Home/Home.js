045982f502e93a445fd1d28614b64facb99f2e49
nkbadhon <badhon7574@gmail.com>
2022-10-11 04:19:37 +0600

    Fetch for all courses done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
-
+import { useLoaderData } from 'react-router-dom';
 const Home = () => {
+    const course = useLoaderData();
+    const { data } = course;
+    console.log(data[0])
     return (
         <div>
             <Header></Header>
-            <h1 className='text-sky-400'>This is Home</h1>
+            <h1 className='text-sky-400'>This is Home:{data.length}</h1>
         </div>
     );
 };
